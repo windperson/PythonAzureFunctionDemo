11f8d77d726155a973c0b4a72b5d4f5dd3898d25
Chen-Yu Pao <windperson@gmail.com>
2020-10-14 11:08:59 +0800

    try upgrade to Azure function runtime v3 and add application insight

--- UploadDemo/__init__.py
+++ UploadDemo/__init__.py
@@ -1,23 +1,28 @@
 import logging
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler())
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logger.info('Python HTTP trigger function processed a request.')
 
     if req.method != 'POST':
         return func.HttpResponse("Use Http Post", status_code=405)
     try:
         for uploadKey, uploadFile in req.files.items():
-            logging.info('\n=====\n')
-            logging.info('Start processing upload file: %s' % (uploadFile.filename))
-            logging.info('file object type: %s' % type(uploadFile))
-            logging.info('file extension = %s' % uploadFile.mimetype)
-            logging.info('file content:')
+            logger.info('\n=====\n')
+            logger.info('Start processing upload file: %s' % (uploadFile.filename))
+            logger.info('file object type: %s' % type(uploadFile))
+            logger.info('file extension = %s' % uploadFile.mimetype)
+            logger.info('file content:')
             content = uploadFile.stream.read()
-            logging.info(content)
+            logger.info(content)
         return func.HttpResponse('done')
     except Exception as e:
-        print(e) 
+        logger.exception(e) 
         return func.HttpResponse('Process file failed, exception= [%s]' % e, status_code=400)  
